cabc0003c69e28c028201022fbd5bc78a0315dd3
Archit Mahule <mahulearchit@gmail.com>
2024-05-30 19:28:58 +0530

    add additional styling for video player

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -15,6 +15,12 @@ const VideoPlayer = ({ playerRef }) => {
   const videoOpts = {
     width: '100%',
     height: '100%',
+    playerVars: {
+      autoplay: 1,
+      controls: 1,
+      modestbranding: 1,
+      rel: 0,
+    },
   };
 
   return (
